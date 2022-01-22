a72723ada1aa3aaae433740eafc1736ba86af343
Michal.Tkaczyk <michaltkaczyk@outlook.com>
2022-01-22 21:33:58 +0100

    init
    first draft for a board definition, plus ways of checking the legality of four moves

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Board:
+
+    def __init__(self, board_state):
+        self.board_state = board_state
+        self.board_shape = np.array(board_state.shape)
+        self.empty_tile_position = np.squeeze(np.argwhere(np.isnan(board_state))) + 1
+
+    def show_available_moves(self):
+        available_moves = list()
+
+        if self.empty_tile_position[0] < self.board_shape[0]:
+            available_moves.append("up")
+
+        if self.empty_tile_position[0] > 0:
+            available_moves.append("down")
+
+        if self.empty_tile_position[1] < self.board_shape[1]:
+            available_moves.append("left")
+
+        if self.empty_tile_position[1] > 0:
+            available_moves.append("right")
+
+        print(available_moves)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    board = Board(np.array([[1, 2, 3], [4, 5, 6], [7, 8, np.nan]]))
+    board.show_available_moves()
